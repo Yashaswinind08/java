34e56e679e64f26728bb4a64d8a9f7f24f034b14
yashaswinind08 <yashaswinind08@gmail.com>
2026-02-20 20:32:46 +0530

    adding two numbers

---
+++ main.java
@@ -0,0 +1,19 @@
+//printing my name
+import java.util.*;
+
+public class main{
+int sum;
+  public static int add(int a,int b){
+    int sum=a+b;
+    return sum;
+    
+  }
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum=add(a,b); //function is called here
+    System.out.println("the sum of a and b is :" + sum);
+    sc.close();
+  }
+}
